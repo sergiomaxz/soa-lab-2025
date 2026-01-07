1b1a670aa30216cf4168c41366f9a3df6e2bde84
sergiomaxz <93317889+sergiomaxz@users.noreply.github.com>
2026-01-07 00:01:21 +0000

    added a bit more logging for analytics service

--- analytics_service/app.py
+++ analytics_service/app.py
@@ -8,6 +8,10 @@ channel = connection.channel()
 channel.queue_declare(queue='ride_finished_queue')
 
 def callback(ch, method, properties, body):
+    data = json.loads(body)
+    print(f" [x] Received Ride ID: {data['ride_id']}")
+    print(f" ... Calculating stats for User {data['user_id']} ...")
+    # Тут була б складна математика
     print(f" [x] Analytics processed ride: {json.loads(body)}")
 
 channel.basic_consume(queue='ride_finished_queue', on_message_callback=callback, auto_ack=True)
